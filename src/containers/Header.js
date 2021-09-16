b161b3b46945b6c5f787a1859135bc15da9b2f26
Abiy-AI <abiy_chung@yahoo.com>
2021-09-16 09:06:54 -0400

    First Commit

---
+++ src/containers/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui fixed menu">
+      <div className="ui container center">
+        <header>
+          <a href="/">React Shopping Cart</a>
+          <main>Product List</main>
+          <footer>All right is reserved</footer>
+        </header>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
